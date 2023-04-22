08e6b737c3f1a06515f8a224085119c10279b6db
Imnmania <niloyb@live.com>
2023-04-22 21:40:22 +0600

    custom painter background example

---
+++ android/app/src/main/kotlin/me/niloybiswas/custom_painter_example_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.niloybiswas.custom_painter_example_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          color: Colors.purple,
+        ),
+        child: CustomPaint(
+          painter: BackgroundPainter(),
+        ),
+      ),
+    );
+  }
+}
+
+class BackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.white.withOpacity(0.1);
+
+    final path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width * 0.2, 0);
+    path.lineTo(0, size.height * 0.4);
+    path.close();
+
+    final path2 = Path();
+    path2.moveTo(size.width, 0);
+    path2.lineTo(size.width * 0.8, 0);
+    path2.lineTo(size.width * 0.2, size.height);
+    path2.lineTo(size.width, size.height);
+    path2.close();
+
+    canvas.drawPath(path, paint);
+    canvas.drawPath(path2, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
